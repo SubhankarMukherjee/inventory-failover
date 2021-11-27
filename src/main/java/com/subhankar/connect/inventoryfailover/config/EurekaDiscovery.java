a59a01b5b042382739baea815a60878ac9a40753
SubhankarMukherjee <connecttosubhankar@gmail.com>
2021-11-27 23:54:06 +0530

    first commit recative

---
+++ src/main/java/com/subhankar/connect/inventoryfailover/config/EurekaDiscovery.java
@@ -0,0 +1,8 @@
+package com.subhankar.connect.inventoryfailover.config;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+@EnableDiscoveryClient
+@Configuration
+public class EurekaDiscovery {
+}
